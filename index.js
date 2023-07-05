0519369244e6f9ee9927f9f8e5ea783019ee3aa2
User083 <97461050+User083@users.noreply.github.com>
2023-07-05 19:38:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const middlewares = jsonServer.defaults()
 server.use(cors())
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 server.use(router)
 
 const PORT = 8000
