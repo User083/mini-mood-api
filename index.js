b6af3d19404e6b46588eadf2a89c12d7484e0fd2
User083 <97461050+User083@users.noreply.github.com>
2023-07-07 09:08:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ server.listen(PORT, () => {
 
 
   server.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
